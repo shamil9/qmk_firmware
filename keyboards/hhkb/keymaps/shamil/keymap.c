964e41a14417ded5cb296923899b3b6d0186c6ad
Shamil A <shamil9@gmail.com>
2019-06-02 09:47:09 +0200

    Update hhkb layout

--- keyboards/hhkb/keymaps/shamil/keymap.c
+++ keyboards/hhkb/keymaps/shamil/keymap.c
@@ -26,7 +26,8 @@ enum {
   FAT_ARROW = 4,
   MORE_THAN = 5,
   LESS_THAN = 6,
-  LAYOUT_2 = 7,
+  GAMING = 7,
+  DEFAULT = 8,
 };
 
 void fat_arrow(qk_tap_dance_state_t *state, void *user_data) {
@@ -72,7 +73,8 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [FAT_ARROW]  = ACTION_TAP_DANCE_FN(fat_arrow),
   [MORE_THAN]  = ACTION_TAP_DANCE_FN(more_than),
   [LESS_THAN]  = ACTION_TAP_DANCE_FN(less_than),
-  [LAYOUT_2]  = ACTION_TAP_DANCE_DUAL_ROLE(KC_MPLY, 2),
+  [GAMING]  = ACTION_TAP_DANCE_DUAL_ROLE(KC_MPLY, 2),
+  [DEFAULT]  = ACTION_TAP_DANCE_DUAL_ROLE(KC_MPLY, 0),
 // Other declarations would go here, separated by commas, if you have them
 };
 
@@ -94,11 +96,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             |------+------+-----------------------+------+------|
     */
 
-    [BASE] = LAYOUT( //  default layer
+    [0] = LAYOUT( //  default layer
         KC_ESC,         KC_1, KC_2, KC_3, KC_4, KC_5, TD(ALT_SIX), KC_7,      KC_8,   KC_9,    KC_0,      KC_MINS, KC_EQL, KC_BSLS, TD(ALT_GRAVE),
         KC_TAB,         KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U,  KC_I,      KC_O,   KC_P,    KC_LBRC,   KC_RBRC, KC_BSPC,
         LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J,  KC_K,      KC_L,   KC_SCLN, TD(ALT_E), KC_ENT,
-        KC_LSFT,        KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M,  TD(ALT_C), KC_DOT, KC_SLSH, KC_RSFT,   KC_MPLY,
+        KC_LSFT,        KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M,  TD(ALT_C), KC_DOT, KC_SLSH, KC_RSFT,   TD(GAMING),
         KC_LALT,        KC_LGUI, /*        */ LT(1, KC_SPACE),     KC_RGUI,   KC_RALT),
 
     /* Layer HHKB: HHKB mode (HHKB Fn)
@@ -118,12 +120,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
      */
 
-    [HHKB] = LAYOUT(
+    [1] = LAYOUT(
         KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS, KC_DEL,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_ENT,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_BSPC,
         KC_CAPS, KC_TRNS, KC_TRNS, KC_BSPC, KC_LBRC, KC_RBRC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LCBR, KC_RCBR, LARW,    RARW,    RARWF,   KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)};
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+
+    [2] = LAYOUT( // win layer
+        KC_ESC,  KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7,  KC_8,      KC_9,   KC_0,    KC_MINS,  KC_EQL,  KC_BSLS, KC_GRAVE,
+        KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U,  KC_I,      KC_O,   KC_P,    KC_LBRC,  KC_RBRC, KC_BSPC,
+        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J,  KC_K,      KC_L,   KC_SCLN, KC_QUOTE, KC_ENT,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M,  KC_COMMA,  KC_DOT, KC_SLSH, KC_RSFT,  TD(DEFAULT),
+        KC_LGUI, KC_LALT, /*        */ KC_SPACE,     KC_RALT,   KC_RGUI)
+  };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
