e7f83251314e0df7cbe551e127dea96b62a4e8ff
Shamil A <shamil9@gmail.com>
2019-08-11 16:20:45 +0200

    Update kbd6x with accents

--- keyboards/kbdfans/kbd6x/keymaps/shamil/keymap.c
+++ keyboards/kbdfans/kbd6x/keymaps/shamil/keymap.c
@@ -32,6 +32,12 @@ enum custom_keycodes {
   RARW = SAFE_RANGE,
   LARW,
   RARWF,
+  EONE,
+  ETWO,
+  AONE,
+  ATWO,
+  UONE,
+  CONE,
 };
 
 
@@ -85,36 +91,52 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-      KC_ESC,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     TD(ALT_SIX), KC_7,    KC_8,      KC_9,   KC_0,    KC_MINS,   KC_EQL,  KC_BSLS, TD(ALT_GRAVE),
-      KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,        KC_U,    KC_I,      KC_O,   KC_P,    KC_LBRC,   KC_RBRC, KC_BSPC,
-      LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,        KC_J,    KC_K,      KC_L,   KC_SCLN, TD(ALT_E), KC_ENT,
-      KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,        KC_M,    TD(ALT_C), KC_DOT, KC_SLSH, KC_RSFT,   TD(GAMING),
-      KC_HYPR,        KC_LALT, KC_LGUI,                   LT(1, KC_SPACE),       KC_RGUI, KC_RALT,   KC_RCTL
+      KC_ESC,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     TD(ALT_SIX), KC_7,    KC_8,      KC_9,    KC_0,    KC_MINS,   KC_EQL,  KC_BSLS, LT(5, KC_GRAVE),
+      KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,        KC_U,    KC_I,      KC_O,    KC_P,    KC_LBRC,   KC_RBRC, KC_BSPC,
+      LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,        KC_J,    KC_K,      KC_L,    KC_SCLN, KC_QUOTE,  KC_ENT,
+      KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,        KC_M,    KC_COMMA,  KC_DOT,  KC_SLSH, KC_RSFT,   TD(GAMING),
+                               KC_HYPR, KC_LALT, KC_LGUI,           LT(1, KC_SPACE),      KC_RGUI,   KC_RALT, KC_RCTL
       ),
 
   [1] = LAYOUT(
       KC_ESC,         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_F6,      KC_F7,   KC_F8,     KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_DEL,
-      KC_TRNS,        RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI,   RGB_SAD,    RGB_VAI, RGB_VAD,   KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS,
-      KC_TRNS,        KC_TRNS, KC_TRNS, KC_BSPC, KC_TRNS, KC_TRNS,   KC_LEFT,    KC_DOWN, KC_UP,     KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS,
-      KC_TRNS,        KC_TRNS, KC_TRNS, BL_DEC,  BL_TOGG, BL_INC,    BL_STEP,    KC_TRNS, LARW,      RARW,    RARWF,   KC_TRNS, RESET,
-      KC_TRNS,        KC_TRNS, KC_TRNS,                   KC_TRNS,               KC_TRNS, KC_TRNS,   KC_TRNS
+      KC_TRNS,        KC_TRNS, KC_TRNS, ETWO,    EONE,    KC_TRNS,   KC_TRNS,    KC_TRNS, UONE,      KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS,
+      KC_TRNS,        ATWO,    AONE,    KC_BSPC, KC_TRNS, KC_TRNS,   KC_LEFT,    KC_DOWN, KC_UP,     KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, BL_INC,    BL_STEP,    KC_TRNS, LARW,      RARW,    RARWF,   KC_TRNS, RESET,
+                               KC_TRNS, KC_TRNS, KC_TRNS,            KC_TRNS,             KC_TRNS,   KC_TRNS, KC_TRNS
       ),
 
   [2] = LAYOUT(
-      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,      KC_9,   KC_0,    KC_MINS,  KC_EQL,  KC_BSLS, KC_GRAVE,
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,      KC_O,   KC_P,    KC_LBRC,  KC_RBRC, KC_BSPC,
-      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,      KC_L,   KC_SCLN, KC_QUOTE, KC_ENT,
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMMA,  KC_DOT, KC_SLSH, KC_RSFT,  TD(DEFAULT),
-      KC_HYPR,          KC_LGUI, KC_LALT,          KC_SPACE,          TT(3),   KC_RGUI,   KC_RCTL
+      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,      KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSLS, KC_GRAVE,
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,      KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSPC,
+      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,    KC_J,    KC_K,      KC_L,    KC_SCLN, KC_QUOTE, KC_ENT,
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMMA,  KC_DOT,  KC_SLSH, KC_RSFT,  TD(DEFAULT),
+                        KC_HYPR, KC_LGUI, KC_LALT,           KC_SPACE,         TT(3),     KC_RGUI, KC_RCTL
       ),
 
   [3] = LAYOUT(
-      KC_ESC,  KC_1,    KC_2,    KC_3,     KC_4,    KC_5,     KC_6,    KC_7,    KC_8,      KC_9,   KC_0,    KC_MINS,  KC_EQL,  KC_BSLS, KC_DEL,
-      KC_TAB,  KC_Q,    KC_UP,   KC_E,     KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,      KC_O,   KC_P,    KC_MPRV,  KC_MNXT, KC_BSPC,
-      KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_F,    KC_G,     KC_H,    KC_J,    KC_K,      KC_L,   KC_SCLN, KC_QUOTE, KC_ENT,
-      KC_LSFT, KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,     KC_N,    KC_M,    KC_COMMA,  KC_DOT, KC_SLSH, KC_RSFT,  KC_TRNS,
-      KC_HYPR,          KC_LGUI, KC_LALT,          KC_SPACE,          KC_TRNS,  KC_RGUI,   KC_RCTL
+      KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_6,    KC_7,    KC_8,      KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSLS, KC_DEL,
+      KC_TAB,  KC_Q,    KC_UP,   KC_E,    KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,      KC_O,    KC_P,    KC_MPRV,  KC_MNXT, KC_BSPC,
+      KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_F,    KC_G,     KC_H,    KC_J,    KC_K,      KC_L,    KC_SCLN, KC_QUOTE, KC_ENT,
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMMA,  KC_DOT,  KC_SLSH, KC_RSFT,  KC_TRNS,
+                        KC_HYPR, KC_LGUI,  KC_LALT,          KC_SPACE,         KC_TRNS,   KC_RGUI, KC_RCTL
       ),
+
+  [4] = LAYOUT(
+      KC_ESC,  KC_1,    KC_2,    KC_3,     KC_4,    KC_5,     KC_6,    KC_7,    KC_8,      KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSLS, KC_DEL,
+      KC_TAB,  KC_Q,    KC_UP,   KC_E,     KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,      KC_O,    KC_P,    KC_MPRV,  KC_MNXT, KC_BSPC,
+      KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT,  KC_F,    KC_G,     KC_H,    KC_J,    KC_K,      KC_L,    KC_SCLN, KC_QUOTE, KC_ENT,
+      KC_LSFT, KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,     KC_N,    KC_M,    KC_COMMA,  KC_DOT,  KC_SLSH, KC_RSFT,  KC_TRNS,
+                        KC_HYPR, KC_LGUI,  KC_LALT,           KC_SPACE,         KC_TRNS,   KC_RGUI, KC_RCTL
+      ),
+
+  [5] = LAYOUT(
+      KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_NO,  KC_NO,     KC_NO,   KC_NO,   RGB_SPD,  RGB_SPI, KC_NO, KC_NO,
+      BL_TOGG, RGB_TOG, RGB_MOD, RGB_RMOD, KC_NO,   KC_NO,    KC_NO,    KC_NO,  KC_NO,     KC_NO,   KC_NO,   KC_NO,    KC_NO,   KC_NO,
+      KC_NO,   RGB_HUI, RGB_HUD, KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_NO,  KC_NO,     KC_NO,   KC_NO,   KC_NO,    KC_NO,
+      KC_NO,   RGB_VAI, RGB_VAD, BL_INC,   BL_DEC,  BL_STEP,  KC_NO,    KC_NO,  KC_NO,     KC_NO,   KC_NO,   KC_NO,    KC_NO,
+                        KC_NO,   KC_NO,    KC_NO,             KC_NO,            KC_NO,     KC_NO,   KC_NO
+      )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -130,6 +152,25 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
             case LARW:
                 SEND_STRING("<-");
+                break;
+            case EONE:
+                SEND_STRING(SS_LALT("e")"e");
+                break;
+
+            case ETWO:
+                SEND_STRING(SS_LALT("`")"e");
+                break;
+
+            case AONE:
+                SEND_STRING(SS_LALT("e")"a");
+                break;
+
+            case ATWO:
+                SEND_STRING(SS_LALT("`")"a");
+                break;
+
+            case CONE:
+                SEND_STRING(SS_LALT("c")"c");
                 break;
         }
     }
