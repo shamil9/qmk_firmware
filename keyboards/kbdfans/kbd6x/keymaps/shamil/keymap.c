e1c9967737165db3cdd4665a0ecec1f5298e3a33
Shamil A <shamil9@gmail.com>
2019-08-16 22:20:32 +0200

    kbd6x update

--- keyboards/kbdfans/kbd6x/keymaps/shamil/keymap.c
+++ keyboards/kbdfans/kbd6x/keymaps/shamil/keymap.c
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT(
       KC_ESC,         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,     KC_F6,      KC_F7,   KC_F8,     KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_DEL,
-      KC_TRNS,        KC_TRNS, KC_TRNS, EONE,    ETWO,    KC_TRNS,   KC_TRNS,    KC_TRNS, UONE,      KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS,
+      KC_TRNS,        KC_TRNS, KC_TRNS, EONE,    ETWO,    KC_TRNS,   KC_TRNS,    UONE, KC_TRNS,   KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS,
       KC_TRNS,        ATWO,    AONE,    KC_BSPC, KC_TRNS, KC_TRNS,   KC_LEFT,    KC_DOWN, KC_UP,     KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS,
       KC_TRNS,        KC_TRNS, KC_TRNS, CONE,    KC_TRNS, BL_INC,    BL_STEP,    KC_TRNS, LARW,      RARW,    RARWF,   KC_TRNS, RESET,
                                KC_TRNS, KC_TRNS, KC_TRNS,            KC_TRNS,             KC_TRNS,   KC_TRNS, KC_TRNS
@@ -171,6 +171,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
             case CONE:
                 SEND_STRING(SS_LALT("c")"c");
+                break;
+
+            case UONE:
+                SEND_STRING(SS_LALT("`")"u");
                 break;
         }
     }
