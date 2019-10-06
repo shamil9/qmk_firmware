19c7208ea5edef2396c7bfa5eb54fa7e87ec4c59
Shamil A <shamil9@gmail.com>
2019-10-06 14:31:04 +0200

    Update dz60

--- keyboards/dz60/keymaps/shamil/keymap.c
+++ keyboards/dz60/keymaps/shamil/keymap.c
@@ -38,11 +38,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_HYPR,           KC_LALT,    KC_LGUI,                      LT(1, KC_SPACE),                     OSL(1),     KC_RGUI,                                       KC_HYPR
 	),
 	[1] = LAYOUT_60_tsangan(
-		KC_ESC,            KC_F1,      KC_F2,      KC_F3,   KC_F4,   KC_F5,   KC_F6,          KC_F7,      KC_F8,      KC_F9,      KC_F10,    KC_F11,     KC_F12,     KC_DEL,
+		KC_GRAVE,          KC_F1,      KC_F2,      KC_F3,   KC_F4,   KC_F5,   KC_F6,          KC_F7,      KC_F8,      KC_F9,      KC_F10,    KC_F11,     KC_F12,     KC_DEL,
         KC_TAB,            KC_Q,       KC_W,       EONE,    ETWO,    KC_T,    KC_Y,           UONE,       KC_I,       KC_O,       KC_P,      KC_MPRV,    KC_MNXT,    KC_BSLS,
         KC_LCTRL,          ATWO,       AONE,       KC_BSPC, KC_F,    KC_G,    KC_LEFT,        KC_DOWN,    KC_UP,      KC_RIGHT,   KC_SCLN,   KC_QUOT,                RESET,
         KC_LSFT,           KC_Z,       KC_X,       CONE,    KC_V,    KC_B,    KC_N,           KC_M,       LARW,       RARW,       RARWF,     KC_RSFT,                TD(GAMING),
-        KC_HYPR,           KC_LGUI,    KC_LALT,                      LT(1, KC_SPACE),                     OSL(1),     KC_RGUI,                                       KC_HYPR
+        KC_HYPR,           KC_LALT,    KC_LGUI,                      LT(1, KC_SPACE),                     OSL(1),     KC_RGUI,                                       KC_HYPR
 	),
     [2] = LAYOUT_60_tsangan(
         KC_ESC,            KC_1,       KC_2,       KC_3,    KC_4,    KC_5,    KC_6,           KC_7,       KC_8,       KC_9,       KC_0,      KC_MINS,    KC_EQL,     KC_BSPC,
