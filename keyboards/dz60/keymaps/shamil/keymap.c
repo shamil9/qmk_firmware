cb610c5f9cab27964a056322bbb956b0fc19c3a7
Shamil A <shamil9@gmail.com>
2019-10-13 10:56:06 +0200

    Update dz60

--- keyboards/dz60/keymaps/shamil/keymap.c
+++ keyboards/dz60/keymaps/shamil/keymap.c
@@ -50,7 +50,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,            KC_Q,       KC_W,       KC_E,    KC_R,    KC_T,    KC_Y,           KC_U,       KC_I,       KC_O,      KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,
         KC_LCTRL,          KC_A,       KC_S,       KC_D,    KC_F,    KC_G,    KC_H,           KC_J,       KC_K,       KC_L,      KC_SCLN,    KC_QUOT,                KC_ENT,
         KC_LSFT,           KC_Z,       KC_X,       KC_C,    KC_V,    KC_B,    KC_N,           KC_M,       KC_COMM,    KC_DOT,    KC_SLSH,    KC_RSFT,                TD(DEFAULT),
-        KC_HYPR,           KC_NO,      KC_LALT,                      KC_SPACE,                            MO(1),      KC_RGUI,                                       KC_HYPR
+        KC_HYPR,           MO(3),      KC_LALT,                      KC_SPACE,                            OSL(3),     KC_RGUI,                                       KC_HYPR
+    ),
+    [3] = LAYOUT_60_tsangan(
+        KC_GRAVE,          KC_F1,      KC_F2,      KC_F3,   KC_F4,   KC_F5,   KC_F6,          KC_F7,      KC_F8,      KC_F9,      KC_F10,    KC_F11,     KC_F12,     KC_DEL,
+        KC_TAB,            KC_Q,       KC_W,       KC_TRNS, KC_TRNS, KC_T,    KC_Y,           KC_TRNS,    KC_TRNS,    KC_O,       KC_P,      KC_MPRV,    KC_MNXT,    KC_BSLS,
+        KC_LCTRL,          KC_TRNS,    KC_TRNS,    KC_BSPC, KC_F,    KC_G,    KC_LEFT,        KC_DOWN,    KC_UP,      KC_RIGHT,   KC_SCLN,   KC_QUOT,                KC_TRNS,
+        KC_LSFT,           KC_Z,       KC_X,       KC_TRNS, KC_V,    KC_B,    KC_N,           KC_M,       LARW,       RARW,       RARWF,     KC_RSFT,                KC_TRNS,
+        KC_HYPR,           KC_TRNS,    KC_TRNS,                      KC_TRNS,                             KC_TRNS,    KC_RGUI,                                       KC_HYPR
     ),
 };
 
