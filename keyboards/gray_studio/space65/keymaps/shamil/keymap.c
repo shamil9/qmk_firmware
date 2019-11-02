92f3df11b183c89aea1f8f4822b7d3e7de70c63f
Shamil A <shamil9@gmail.com>
2019-11-02 09:30:59 +0100

    Update space65

--- keyboards/gray_studio/space65/keymaps/shamil/keymap.c
+++ keyboards/gray_studio/space65/keymaps/shamil/keymap.c
@@ -9,20 +9,15 @@ enum custom_keycodes {
   AONE,
   ATWO,
   UONE,
+  UTWO,
   CONE,
 };
 
 //Tap Dance Declarations
 enum {
-  ALT_E = 0,
-  ALT_SIX = 1,
-  ALT_GRAVE = 2,
-  ALT_C = 3,
-  FAT_ARROW = 4,
-  MORE_THAN = 5,
-  LESS_THAN = 6,
-  GAMING = 7,
-  DEFAULT = 8,
+  ALT_SIX = 0,
+  GAMING = 1,
+  DEFAULT = 2,
 };
 
 void fat_arrow(qk_tap_dance_state_t *state, void *user_data) {
@@ -61,13 +56,7 @@ void less_than(qk_tap_dance_state_t *state, void *user_data) {
 //Tap Dance Definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
   //Tap once for Esc, twice for Caps Lock
-  [ALT_E]  = ACTION_TAP_DANCE_DOUBLE(KC_QUOTE, LALT(KC_E)),
   [ALT_SIX]  = ACTION_TAP_DANCE_DOUBLE(KC_6, LALT(KC_6)),
-  [ALT_GRAVE]  = ACTION_TAP_DANCE_DOUBLE(KC_GRAVE, LALT(KC_GRAVE)),
-  [ALT_C]  = ACTION_TAP_DANCE_DOUBLE(KC_COMMA, LALT(KC_C)),
-  [FAT_ARROW]  = ACTION_TAP_DANCE_FN(fat_arrow),
-  [MORE_THAN]  = ACTION_TAP_DANCE_FN(more_than),
-  [LESS_THAN]  = ACTION_TAP_DANCE_FN(less_than),
   [GAMING]  = ACTION_TAP_DANCE_DUAL_ROLE(KC_MPLY, 2),
   [DEFAULT]  = ACTION_TAP_DANCE_DUAL_ROLE(KC_MPLY, 0),
 // Other declarations would go here, separated by commas, if you have them
@@ -75,19 +64,19 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
-        KC_ESC,         KC_1,  KC_2, KC_3, KC_4, KC_5, TD(ALT_SIX), KC_7, KC_8, KC_9,      KC_0,    KC_MINS,   KC_EQL,  KC_BSPC, KC_BSPC, TD(ALT_GRAVE),
+        KC_ESC,         KC_1,  KC_2, KC_3, KC_4, KC_5, TD(ALT_SIX), KC_7, KC_8, KC_9,      KC_0,    KC_MINS,   KC_EQL,  KC_BSPC, KC_BSPC, KC_GRAVE,
         KC_TAB,         KC_Q,  KC_W, KC_E, KC_R, KC_T, KC_Y,        KC_U, KC_I, KC_O,      KC_P,    KC_LBRC,   KC_RBRC, KC_BSLS,          LT(3, KC_PGUP),
         LCTL_T(KC_ESC), KC_A,  KC_S, KC_D, KC_F, KC_G, KC_H,        KC_J, KC_K, KC_L,      KC_SCLN, KC_QUOTE,  KC_ENT,                    KC_PGDN,
         KC_LSFT,        KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B,        KC_N, KC_M, KC_COMMA,  KC_DOT,  KC_SLSH,   KC_RSFT, KC_UP,            TD(GAMING),
-        KC_HYPR,        KC_LALT, KC_LGUI, KC_SPC, LT(1, KC_SPACE), KC_SPC, OSL(1), KC_RALT,        KC_LEFT,   KC_DOWN, KC_RGHT
+        KC_HYPR,        KC_LALT, KC_LGUI, KC_SPC, LT(1, KC_SPACE), KC_SPC, OSL(1), KC_RALT,         KC_LEFT,   KC_DOWN, KC_RGHT
     ),
 
 	[1] = LAYOUT(
-        KC_NO, KC_F1, KC_F2, KC_F3,   KC_F4, KC_F5, KC_F6,   KC_F7,   KC_F8, KC_F9,   KC_F10, KC_F11,  KC_F12,  KC_NO, KC_NO, KC_DEL,
-        KC_NO, KC_NO, KC_NO, EONE,    ETWO,  KC_NO, KC_NO,   UONE,    KC_NO, KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_NO,        KC_NO,
-        KC_NO, ATWO,  AONE,  KC_BSPC, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO,  KC_NO,   KC_NO,                 KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO,   CONE,  KC_NO, KC_NO,   KC_NO,   KC_NO, LARW,    RARW,   RARWF,   KC_VOLU, KC_NO,        RESET,
-        KC_NO,               KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,               KC_MPRV, KC_VOLD, KC_MNXT
+        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_DEL,
+        KC_TRNS, KC_TRNS, KC_TRNS, EONE,    ETWO,    KC_TRNS, KC_TRNS, UONE,    UTWO,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
+        KC_TRNS, ATWO,    AONE,    KC_BSPC, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,
+        KC_TRNS, KC_NO,   KC_TRNS, KC_TRNS, CONE,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LARW,    RARW,    RARWF,   KC_VOLU, KC_TRNS,          RESET,
+        KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_MPRV, KC_VOLD, KC_MNXT
     ),
 
     [2] = LAYOUT(
@@ -95,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,  KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O,     KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS,          KC_PGUP,
         KC_LCTL, KC_A,  KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,     KC_SCLN, KC_QUOTE, KC_ENT,                    KC_PGDN,
         KC_LSFT, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT,  KC_SLSH,  KC_RSFT, KC_UP,            TD(DEFAULT),
-        KC_HYPR,       KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_RALT, KC_RGUI,            KC_LEFT, KC_DOWN, KC_RGHT
+        KC_HYPR,        KC_NO,   KC_LALT, KC_SPC, KC_SPC, KC_SPC, OSL(4),  KC_RGUI,            KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [3] = LAYOUT(
@@ -104,6 +93,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,   RGB_HUI, RGB_HUD, KC_NO,    KC_NO,  KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,                 KC_NO,
         KC_NO,   KC_NO,   RGB_VAI, RGB_VAD,  BL_INC, BL_DEC,  BL_STEP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,        KC_NO,
         KC_NO,            KC_NO,   KC_NO,    KC_NO,  KC_NO,   KC_NO, KC_NO, KC_NO,                 KC_NO,   KC_NO,   KC_NO
+    ),
+
+    [4] = LAYOUT(
+        KC_TRNS, KC_F1, KC_F2,KC_F3,KC_F4,KC_F5,KC_F6,KC_F7,KC_F8,KC_F9,    KC_F10,  KC_F11,   KC_F12,  KC_TRNS, KC_TRNS, KC_DEL,
+        KC_TAB,  KC_Q,  KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O,     KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS,          KC_PGUP,
+        KC_LCTL, KC_A,  KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,     KC_SCLN, KC_QUOTE, KC_ENT,                    KC_PGDN,
+        KC_LSFT, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT,  KC_SLSH,  KC_RSFT, KC_UP,            KC_TRNS,
+        KC_HYPR,        KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_RALT, KC_RGUI,            KC_LEFT, KC_DOWN, KC_RGHT
     ),
 };
 
@@ -144,6 +141,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
         case UONE:
           SEND_STRING(SS_LALT("`")"u");
+          break;
+
+        case UTWO:
+          SEND_STRING(SS_LALT("e")"u");
           break;
     }
   }
