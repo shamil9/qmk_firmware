3f1ca447f404e960f901c44da72e896ee06304e3
Shamil A <shamil9@gmail.com>
2019-12-23 20:33:07 +0100

    Update m60-a

--- keyboards/wilba_tech/rama_works_m60_a/keymaps/shamil/keymap.c
+++ keyboards/wilba_tech/rama_works_m60_a/keymaps/shamil/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Default layer
 [0] = LAYOUT_60_hhkb(
-	KC_ESC,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    TD(ALT_SIX),    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_DEL,
+	KC_ESC,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    TD(ALT_SIX),    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRAVE,
 	KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,           KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
 	LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,           KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
 	KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,           KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, TD(GAMING),
@@ -43,8 +43,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Fn1 Layer
 [1] = LAYOUT_60_hhkb(
-    KC_GRAVE,      KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,          KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_DEL,
-    RGB_TOG,       RGB_RMOD, RGB_MOD, EONE,    ETWO,    KC_T,    KC_Y,           UONE,    UTWO,    KC_O,     KC_P,    KC_MPRV, KC_MNXT, KC_BSLS,
+    KC_GRAVE,      KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,          KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_BSLS, KC_GRAVE,
+    RGB_TOG,       RGB_RMOD, RGB_MOD, EONE,    ETWO,    KC_T,    KC_Y,           UONE,    UTWO,    KC_O,     KC_P,    KC_MPRV, KC_MNXT, KC_DEL,
     KC_LCTRL,      ATWO,     AONE,    KC_BSPC, KC_F,    KC_G,    KC_LEFT,        KC_DOWN, KC_UP,   KC_RIGHT, KC_SCLN, KC_QUOT, KC_ENT,
     KC_LSFT,       KC_Z,     KC_X,    CONE,    KC_V,    KC_B,    KC_N,           KC_M,    LARW,    RARW,     RARWF,   KC_RSFT, RESET,
                    KC_LALT,  KC_LGUI,                            KC_TRNS,                  OSL(1),   KC_RGUI
