8cbfcae803aad0108a16d26f38e2386b3c60cb3d
Shamil A <shamil9@gmail.com>
2019-12-24 19:58:34 +0100

    m60-a update

--- keyboards/wilba_tech/rama_works_m60_a/keymaps/shamil/keymap.c
+++ keyboards/wilba_tech/rama_works_m60_a/keymaps/shamil/keymap.c
@@ -52,8 +52,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Fn2 Layer
 [2] = LAYOUT_60_hhkb(
-    KC_ESC,        KC_1,      KC_2,    KC_3,    KC_4,    KC_5,   KC_6,           KC_7,    KC_8,    KC_9,     KC_0,    KC_MINS, KC_EQL,   KC_BSPC, KC_BSPC,
-    KC_TAB,        KC_Q,      KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,           KC_U,    KC_I,    KC_O,     KC_P,    KC_LBRC, KC_RBRC,  KC_BSLS,
+    KC_ESC,        KC_1,      KC_2,    KC_3,    KC_4,    KC_5,   KC_6,           KC_7,    KC_8,    KC_9,     KC_0,    KC_MINS, KC_EQL,   KC_BSLS, KC_GRAVE,
+    KC_TAB,        KC_Q,      KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,           KC_U,    KC_I,    KC_O,     KC_P,    KC_LBRC, KC_RBRC,  KC_BSPC,
     KC_LCTRL,      KC_A,      KC_S,    KC_D,    KC_F,    KC_G,   KC_H,           KC_J,    KC_K,    KC_L,     KC_SCLN, KC_QUOT, KC_ENT,
     KC_LSFT,       KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,   KC_N,           KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT, TD(DEFAULT),
                    MO(3),     KC_LALT,                           KC_SPC,                           OSL(3),   KC_RALT
@@ -61,8 +61,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Fn3 Layer (zeal60 Configuration)
 [3] = LAYOUT_60_hhkb(
-    KC_GRAVE,      KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,         KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,   KC_DEL, KC_DEL,
-    KC_TAB,        KC_Q,      KC_W,    KC_TRNS, KC_TRNS, KC_T,    KC_Y,          KC_TRNS, KC_TRNS, KC_O,     KC_P,    KC_MPRV, KC_MNXT,  KC_BSLS,
+    KC_GRAVE,      KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,         KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,   KC_BSLS, KC_GRAVE,
+    KC_TAB,        KC_Q,      KC_W,    KC_TRNS, KC_TRNS, KC_T,    KC_Y,          KC_TRNS, KC_TRNS, KC_O,     KC_P,    KC_MPRV, KC_MNXT,  KC_DEL,
     KC_LCTRL,      KC_TRNS,   KC_TRNS, KC_TRNS, KC_F,    KC_G,    KC_LEFT,       KC_DOWN, KC_UP,   KC_RIGHT, KC_SCLN, KC_QUOT, KC_TRNS,
     KC_LSFT,       KC_Z,      KC_X,    KC_TRNS, KC_V,    KC_B,    KC_N,          KC_M,    LARW,    RARW,     RARWF,   KC_RSFT, TD(GAMING),
                    KC_TRNS,   KC_TRNS,                            KC_TRNS,                         KC_TRNS,  KC_TRNS
