87ed5b299be42132158a978c4b8ea717d16b27fb
Shamil A <shamil9@gmail.com>
2020-01-26 15:01:03 +0100

    Update wonderland

--- keyboards/maartenwut/wonderland/keymaps/shamil/keymap.c
+++ keyboards/maartenwut/wonderland/keymaps/shamil/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_PGUP,    KC_TAB,         KC_Q,       KC_W,       KC_E,     KC_R,       KC_T,       KC_Y,   KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC, KC_BSPC, \
         KC_PGDN,    LCTL_T(KC_ESC), KC_A,       KC_S,       KC_D,     KC_F,       KC_G,       KC_H,   KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    KC_ENT, \
                     KC_LSFT,        KC_Z,       KC_X,       KC_C,     KC_V,       KC_B,       OSL(1), KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT, TD(GAMING), \
-                    KC_HYPR,        KC_LGUI,    LT(1, KC_SPC),                    KC_LALT,    KC_SPC,                               KC_RGUI,                            KC_HYPR \
+                    KC_LALT,        KC_LGUI,    LT(1, KC_SPC),                    OSL(1),    KC_SPC,                               KC_RGUI,                       KC_HYPR \
     ),
 
     [1] = LAYOUT(
