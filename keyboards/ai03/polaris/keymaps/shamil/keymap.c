062db59eb9d8b67681103bdc4ffdc1d6fd3f163f
Shamil A <shamil9@gmail.com>
2020-07-15 08:54:26 +0200

    update polaris layout

--- keyboards/ai03/polaris/keymaps/shamil/keymap.c
+++ keyboards/ai03/polaris/keymaps/shamil/keymap.c
@@ -21,6 +21,7 @@ enum custom_keycodes {
   RARWF,
   EONE,
   ETWO,
+  ETHREE,
   AONE,
   ATWO,
   UONE,
@@ -56,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT_all( /* FN */
   KC_GRAVE,         KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,          KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS,
-  KC_TRNS,          KC_TRNS,  KC_TRNS, EONE,    ETWO,    KC_TRNS, KC_TRNS,        UONE,    UTWO,    KC_TRNS,  KC_TRNS, KC_MPRV, KC_MNXT,          KC_DEL,
+  KC_TRNS,          KC_TRNS,  KC_TRNS, EONE,    ETWO,    ETHREE,  KC_TRNS,        UONE,    UTWO,    KC_TRNS,  KC_TRNS, KC_MPRV, KC_MNXT,          KC_DEL,
   KC_LCTRL,         ATWO,     AONE,    KC_BSPC, KC_TRNS, KC_TRNS, KC_LEFT,        KC_DOWN, KC_UP,   KC_RIGHT, KC_TRNS, KC_TRNS,          KC_TRNS,
   KC_TRNS,          KC_TRNS,  KC_TRNS, KC_TRNS, CONE,    KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, LARW,     RARW,    RARWF,            KC_TRNS, RESET,
   _______,          _______,  _______,          _______,          _______,                 _______,                    _______, _______, _______, _______
@@ -100,6 +101,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
         case ETWO:
           SEND_STRING(SS_LALT("`")"e");
+          break;
+
+        case ETHREE:
+          SEND_STRING(SS_LALT("6")"e");
           break;
 
         case AONE:
