6147b34b6c4453742f3e85019ba86e0273de1005
Shamil A <shamil9@gmail.com>
2021-06-14 10:01:48 +0200

    update blocked65 layout

--- keyboards/blocked65/keymaps/shamil/keymap.c
+++ keyboards/blocked65/keymaps/shamil/keymap.c
@@ -9,9 +9,10 @@ enum custom_keycodes {
   ETHREE,
   AONE,
   ATWO,
-  UONE,
-  UTWO,
   CONE,
+  UONE,
+  IONE,
+  OONE,
 };
 
 //Tap Dance Declarations
@@ -33,15 +34,15 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-      KC_ESC,           KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,   KC_7,        KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,    KC_GRAVE, \
+      KC_ESC,           KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,          KC_7,        KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,    KC_GRAVE, \
       KC_TAB,           KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,          KC_U,        KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_PGUP,    \
       LCTL_T(KC_ESC),   KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,          KC_J,        KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    KC_ENT,                 KC_PGDN,   \
       KC_LSFT,          KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,          KC_M,        KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,    KC_UP,                  TD(GAMING),   \
       KC_HYPR,          KC_LALT,    KC_LGUI,                LT(1, KC_SPACE),                       OSL(1),      KC_HYPR,                            KC_LEFT,    KC_DOWN,    KC_RIGHT),
 
   [1] = LAYOUT(
-      KC_TRNS,          KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,          KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_DEL,      KC_DEL,   \
-      KC_TRNS,          KC_TRNS,    KC_TRNS,    EONE,       ETWO,       ETHREE,    KC_TRNS,        UONE,       UTWO,       KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,      KC_TRNS,   \
+      RGB_TOG,          KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,          KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_DEL,      KC_DEL,   \
+      RGB_MODE_FORWARD, KC_TRNS,    KC_TRNS,    EONE,       ETWO,       ETHREE,     KC_TRNS,        UONE,       IONE,       OONE,       KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,      KC_TRNS,   \
       KC_TRNS,          ATWO,       AONE,       KC_BSPC,    KC_TRNS,    KC_TRNS,    KC_LEFT,        KC_DOWN,    KC_UP,      KC_RGHT,    KC_TRNS,    KC_TRNS,    KC_TRNS,                  KC_TRNS,   \
       KC_TRNS,          KC_TRNS,    KC_TRNS,    CONE,       KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS,    LARW,       RARW,       RARWF,      KC_VOLU,    KC_VOLU,                  RESET,    \
       KC_TRNS,          KC_TRNS,    KC_TRNS,                KC_TRNS,                                KC_TRNS,    KC_TRNS,                            KC_MPRV,    KC_VOLD,    KC_MNXT),
@@ -90,8 +91,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           SEND_STRING(SS_LALT("e")"u");
           break;
 
-        case UTWO:
-          SEND_STRING(SS_LALT("`")"u");
+        case IONE:
+          SEND_STRING(SS_LALT("6")"i");
+          break;
+
+        case OONE:
+          SEND_STRING(SS_LALT("6")"o");
           break;
     }
   }
