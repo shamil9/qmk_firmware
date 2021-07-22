1dfb021aaca07887efc91bc1aa26703ad755157a
shamil9 <shamil9@gmail.com>
2021-07-22 17:52:03 +0200

    tweak measa tkl layout

--- keyboards/mesa/mesa_tkl/keymaps/shamil/keymap.c
+++ keyboards/mesa/mesa_tkl/keymaps/shamil/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_DEL,  KC_END,  KC_PGDN,
             LCTL_T(KC_ESC),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
             KC_LSFT, _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT, _______,                   KC_UP,
-            KC_LALT, _______, KC_LGUI,                            LT(2, KC_SPC),                               KC_RGUI, _______, TO(2),            KC_LEFT, KC_DOWN, KC_RGHT
+            KC_LALT, _______, KC_LGUI,                            LT(2, KC_SPC),                               KC_RGUI, _______, TO(1),            KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [1] = LAYOUT_all(
             KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    KC_PSCR, KC_SLCK, KC_MPLY,
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_DEL,  KC_END,  KC_PGDN,
             KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
             KC_LSFT, _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT, _______,                   KC_UP,
-            KC_LCTL, _______, KC_LALT,                            KC_SPC,                                      KC_RALT, _______, TO(0),            KC_LEFT, KC_DOWN, KC_RGHT
+            MO(2),   _______, KC_LALT,                            KC_SPC,                                      KC_RALT, _______, TO(0),            KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [2] = LAYOUT_all(
             RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS,
