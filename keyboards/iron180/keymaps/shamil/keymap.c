8088ea5a1ddb099b44308cad332d749e0d86e916
shamil9 <shamil9@gmail.com>
2021-12-11 18:37:39 +0100

    chore: update iron180 layout

--- keyboards/iron180/keymaps/shamil/keymap.c
+++ keyboards/iron180/keymaps/shamil/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS,             KC_DEL , KC_END , KC_PGDN,
  LCTL_T(KC_ESC), KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT ,
         KC_LSFT, KC_BSLS, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_LSFT, MO(1)  ,                      KC_UP  ,
-        KC_LALT, KC_LWIN, KC_LWIN,                            LT(2, KC_SPC) ,                     KC_RALT, KC_RWIN, TO(1)  , KC_RCTL,             KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LALT, KC_LWIN, KC_LWIN,                            LT(2, KC_SPC) ,                     KC_RALT, KC_RWIN, KC_RCTL, TO(1),             KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [1] = LAYOUT_all(
         KC_ESC , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_MPLY ,            KC_PSCR, KC_SLCK, KC_PAUS,
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS,             KC_DEL , KC_END , KC_PGDN,
  LCTL_T(KC_ESC), KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT ,
         KC_LSFT, KC_BSLS, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_LSFT, MO(1)  ,                      KC_UP  ,
-        KC_LCTL, KC_LWIN, KC_LALT,                            KC_SPC ,                            KC_RWIN, KC_RWIN, TO(0)  , KC_RCTL,             KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LWIN, KC_LALT,                            KC_SPC ,                            KC_RWIN, KC_RWIN, KC_RCTL, TO(0),             KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [2] = LAYOUT_all(
         RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
