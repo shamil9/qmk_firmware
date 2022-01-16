6e749b18f8cf20e110a2b62fea2cd79a4413a50b
shamil9 <shamil9@gmail.com>
2022-01-16 17:06:14 +0100

    add meridian layout

---
+++ keyboards/primekb/meridian/keymaps/shamil/keymap.c
@@ -0,0 +1,43 @@
+/*
+Copyright 2020 Holten Campbell
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+#include "shamil.h"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT_split_bs_rshift(
+		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRAVE,
+		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSPC,
+ LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ENT,
+		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,          KC_MPLY,
+		KC_LCTL, KC_LGUI,                   KC_LALT, LT(1, KC_SPACE),  LT(1, KC_SPACE),  KC_RALT,                   KC_RGUI, KC_RCTL, TO(1)
+    ),
+    [1] = LAYOUT_split_bs_rshift(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        MO(2),   _______,                   _______, KC_SPACE,         KC_SPACE,         _______,                   _______, _______, TO(0)
+    ),
+    [2] = LAYOUT_split_bs_rshift(
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
+        UC_MOD,  _______, _______, X(0),    X(2),    X(4),    _______, X(24),   X(14),   X(22),   _______, KC_MPRV, KC_MNXT,          KC_DEL,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
+        _______, _______, _______, X(12),   _______, _______, _______, _______, _______, _______, LARW,    RARW,    RARWF,            TD(DEFAULT),
+        _______, _______,                   _______, _______,          _______,          _______,                   _______, _______, _______
+    ),
+};
