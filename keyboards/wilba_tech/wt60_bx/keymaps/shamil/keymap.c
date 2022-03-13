6eca2198b89a1e68923cca63b75eef8b174f1093
shamil9 <shamil9@gmail.com>
2022-03-13 11:24:19 +0100

    update thermal layout

--- keyboards/wilba_tech/wt60_bx/keymaps/shamil/keymap.c
+++ keyboards/wilba_tech/wt60_bx/keymaps/shamil/keymap.c
@@ -6,19 +6,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Default layer
 [0] = LAYOUT_all(
-    KC_ESC,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,           KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRAVE,
-    KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,           KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
-    LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,           KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
-    KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,           KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT, TD(GAMING),
-	KC_NO,          KC_LALT, KC_LGUI,                            LT(2, KC_SPACE),                  KC_RGUI, TO(1),   KC_NO),
+    KC_ESC,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,           KC_7,    KC_8,    KC_9,    KC_0,        KC_MINS, KC_EQL,  KC_BSLS, KC_GRAVE,
+    KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,           KC_U,    KC_I,    KC_O,    KC_P,        KC_LBRC, KC_RBRC, KC_BSPC,
+    LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,           KC_J,    KC_K,    KC_L,    KC_SCLN,     KC_QUOT, KC_ENT,
+    KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,           KC_M,    KC_COMM, KC_DOT,  KC_SLSH,     KC_LSFT, KC_MPLY,
+	KC_NO,          KC_LALT, KC_LGUI,                            LT(2, KC_SPACE),                  KC_RGUI, WIN_LAYOUT,  KC_NO),
 
 // Fn1 Layer
 [1] = LAYOUT_all(
-    KC_ESC,        KC_1,      KC_2,    KC_3,    KC_4,    KC_5,   KC_6,           KC_7,    KC_8,    KC_9,     KC_0,    KC_MINS, KC_EQL,   KC_BSLS, KC_GRAVE,
-    KC_TAB,        KC_Q,      KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,           KC_U,    KC_I,    KC_O,     KC_P,    KC_LBRC, KC_RBRC,  KC_BSPC,
-    KC_LCTRL,      KC_A,      KC_S,    KC_D,    KC_F,    KC_G,   KC_H,           KC_J,    KC_K,    KC_L,     KC_SCLN, KC_QUOT, KC_ENT,
-    KC_LSFT,       KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,   KC_N,           KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_LSFT, TD(DEFAULT),
-	KC_TRNS,       MO(2),     KC_LALT,                           KC_SPC,                           KC_RGUI,  TO(0),   KC_TRNS),
+    KC_ESC,        KC_1,      KC_2,    KC_3,    KC_4,    KC_5,   KC_6,           KC_7,    KC_8,    KC_9,     KC_0,       KC_MINS, KC_EQL,   KC_BSLS, KC_GRAVE,
+    KC_TAB,        KC_Q,      KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,           KC_U,    KC_I,    KC_O,     KC_P,       KC_LBRC, KC_RBRC,  KC_BSPC,
+    KC_LCTRL,      KC_A,      KC_S,    KC_D,    KC_F,    KC_G,   KC_H,           KC_J,    KC_K,    KC_L,     KC_SCLN,    KC_QUOT, KC_ENT,
+    KC_LSFT,       KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,   KC_N,           KC_M,    KC_COMM, KC_DOT,   KC_SLSH,    KC_LSFT, KC_MPLY,
+	KC_TRNS,       MO(2),     KC_LALT,                           KC_SPC,                           KC_RGUI,  MAC_LAYOUT, KC_TRNS),
 
 // Fn2 Layer
 [2] = LAYOUT_all(
