20976817c3928c76096ca90a6628ce0bb1f7daa8
shamil9 <shamil9@gmail.com>
2022-04-12 16:13:59 +0200

    update uni660 layout

--- keyboards/sirius/uni660/rev2/ansi/keymaps/shamil/rules.mk
+++ keyboards/sirius/uni660/rev2/ansi/keymaps/shamil/rules.mk
@@ -1,1 +1,5 @@
 NKRO_ENABLE = no		# USB Nkey Rollover - not yet supported in LUFA
+MOUSEKEY_ENABLE = no        # Mouse keys
+CONSOLE_ENABLE = no        # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
+CUSTOM_MATRIX = yes
