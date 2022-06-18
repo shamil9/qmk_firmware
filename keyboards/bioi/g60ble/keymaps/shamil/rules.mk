a599fc9caa5e87823a361795528c528e56421723
shamil9 <shamil9@gmail.com>
2022-06-18 23:29:49 +0200

    update vega hexs

---
+++ keyboards/bioi/g60ble/keymaps/shamil/rules.mk
@@ -0,0 +1,7 @@
+NKRO_ENABLE = no
+BLUETOOTH_ENABLE = yes
+BLUETOOTH_DRIVER = RN42 # or RN42
+MOUSEKEY_ENABLE = no      # Mouse keys
+BACKLIGHT_ENABLE = no     # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no
+LTO_ENABLE = yes
