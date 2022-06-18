945ca1b912f02f82add7dd12f94eb4aa31c61398
shamil9 <shamil9@gmail.com>
2022-06-18 23:27:50 +0200

    update 7v layout

--- keyboards/wilba_tech/wt75_c/keymaps/shamil/keymap.c
+++ keyboards/wilba_tech/wt75_c/keymaps/shamil/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
 		KC_LCTL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,
-		KC_TRNS, OSL(2),  KC_LALT,                            KC_SPC,                             KC_TRNS, MAC_LAYOUT,       KC_TRNS, KC_TRNS, KC_TRNS),
+		KC_LGUI, OSL(2),  KC_LALT,                            KC_SPC,                             KC_RGUI, MAC_LAYOUT,       KC_TRNS, KC_TRNS, KC_TRNS),
 
 	// Fn2 Layer
 	[2] = LAYOUT_all(
