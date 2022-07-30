f7a3f39aaa1647d821114b249bd2157365c32a8d
shamil9 <shamil9@gmail.com>
2022-07-30 15:04:38 +0200

    update frog rgb settings

--- keyboards/hineybush/h88/keymaps/shamil/rules.mk
+++ keyboards/hineybush/h88/keymaps/shamil/rules.mk
@@ -1,2 +1,4 @@
 MOUSEKEY_ENABLE = no       # Mouse keys
-NKRO_ENABLE = yes            # Enable N-Key Rollover
+NKRO_ENABLE = yes          # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no     # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no      # Enable RGB underglow
