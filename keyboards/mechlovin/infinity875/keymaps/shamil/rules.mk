af52eaea0d3ccf684afa7f2c5ecf487a0e4b06c0
shamil9 <shamil9@gmail.com>
2022-09-15 20:35:44 +0200

    add mechlovin infinity 875 layout

---
+++ keyboards/mechlovin/infinity875/keymaps/shamil/rules.mk
@@ -0,0 +1,5 @@
+RGB_MATRIX_CUSTOM_USER = no
+MOUSEKEY_ENABLE = no       # Mouse keys
+NKRO_ENABLE = yes          # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no     # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no      # Enable RGB underglow
