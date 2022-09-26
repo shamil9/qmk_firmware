c77efda52be0069fa646e7c2cbaed678241ae2a5
shamil9 <shamil9@gmail.com>
2022-09-26 15:08:19 +0200

    update mechlovin infinity875 layout

--- keyboards/mechlovin/infinity875/keymaps/shamil/keymap.c
+++ keyboards/mechlovin/infinity875/keymaps/shamil/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,    KC_RBRC,    KC_BSLS,                 KC_DEL,  KC_END,  KC_PGDN,
 LCTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,                HYPR_T(KC_ENT),
         KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,    KC_LSFT,    KC_LSFT,                          KC_UP,
-        KC_LALT, KC_NO,   KC_LGUI,                   LT(2, KC_SPC),             KC_RALT, KC_NO,   KC_NO,    WIN_LAYOUT,                                      KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LALT, KC_NO,   KC_LGUI,                   LT(2, KC_SPC),             KC_NO,   KC_RALT, KC_NO,    WIN_LAYOUT,                                      KC_LEFT, KC_DOWN, KC_RGHT),
 
     [1] = LAYOUT_all(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,     KC_F12,     KC_MPLY,                 KC_PSCR, KC_SLCK, KC_PAUS,
@@ -31,7 +31,7 @@ LCTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,    KC_RBRC,    KC_BSLS,                 KC_DEL,  KC_END,  KC_PGDN,
 LCTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,                KC_ENT,
         KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,    KC_LSFT,    KC_LSFT,                           KC_UP,
-        MO(2),   KC_NO,   KC_LALT,                   KC_SPC,                    KC_RGUI, KC_NO,   KC_NO,    MAC_LAYOUT,                                       KC_LEFT, KC_DOWN, KC_RGHT),
+        MO(2),   KC_NO,   KC_LALT,                   KC_SPC,                    KC_NO,   KC_RGUI, KC_NO,    MAC_LAYOUT,                                       KC_LEFT, KC_DOWN, KC_RGHT),
 
     [2] = LAYOUT_all(
         KC_TRNS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,    _______,                 _______, _______, RGB_TOG,
