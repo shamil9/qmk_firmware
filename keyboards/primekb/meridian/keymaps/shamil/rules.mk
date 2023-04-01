8dfc8e08d8085bf559f49da6efb37c9e5d4563b0
shamil9 <shamil9@gmail.com>
2023-04-01 15:23:24 +0200

    update meridian config

--- keyboards/primekb/meridian/keymaps/shamil/rules.mk
+++ keyboards/primekb/meridian/keymaps/shamil/rules.mk
@@ -1,5 +1,4 @@
 MOUSEKEY_ENABLE = no       # Mouse keys
 CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
-DEBOUNCE = 12
-DEBOUNCE_TYPE = sym_eager_pk
+DEBOUNCE_TYPE = sym_defer_g
